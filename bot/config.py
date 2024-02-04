9421a02478503497516da4f8912979a1c02e1b07
Ashish  Vasant Yesale <67554129+AshishYesale7@users.noreply.github.com>
2024-02-04 20:45:26 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -11,7 +11,7 @@ class Telegram:
     SECRET_CODE_LENGTH = int(env.get("SECRET_CODE_LENGTH", 12))
 
 class Server:
-    BASE_URL = env.get("BASE_URL", "https://filestream-h-stream.koyeb.app/")
+    BASE_URL = env.get("BASE_URL", "https://whispering-brook-15151-caf2581340e6.herokuapp.com/")
     BIND_ADDRESS = env.get("BIND_ADDRESS", "0.0.0.0")
     PORT = int(env.get("PORT", 8080))
 
