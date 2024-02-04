330099d4baeabdd409099a57b03d0429891d1911
Ashish  Vasant Yesale <67554129+AshishYesale7@users.noreply.github.com>
2024-02-04 20:17:07 +0530

    Update config.py
    url and vlaue updated

--- bot/config.py
+++ bot/config.py
@@ -1,17 +1,17 @@
 from os import environ as env
 
 class Telegram:
-    API_ID = int(env.get("TELEGRAM_API_ID", 12637827))
-    API_HASH = env.get("TELEGRAM_API_HASH", "bd6f6b7a42292b9f165ae75c55bdf11f")
-    OWNER_ID = int(env.get("OWNER_ID", 5795704298))
-    ALLOWED_USER_IDS = env.get("ALLOWED_USER_IDS", "").split()
-    BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "linktofile222bot")
-    BOT_TOKEN = env.get("TELEGRAM_BOT_TOKEN", "6861902080:AAFI0hRntCXYt61vYauzqQS5ZZ8eZs8yQZI")
-    CHANNEL_ID = int(env.get("TELEGRAM_CHANNEL_ID", -1001995022603))
+    API_ID = int(env.get("TELEGRAM_API_ID", 27248258))
+    API_HASH = env.get("TELEGRAM_API_HASH", "7bddc3e66acfc2679592796cecb9eb8a")
+    OWNER_ID = int(env.get("OWNER_ID", 6708682548))
+    ALLOWED_USER_IDS = env.get("ALLOWED_USER_IDS", "6708682548").split()
+    BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "hstream_file2stream_bot")
+    BOT_TOKEN = env.get("TELEGRAM_BOT_TOKEN", "6940035846:AAGNjrQo1z6PSJPjjbEpqBUt1u1KqoraxXM")
+    CHANNEL_ID = int(env.get("TELEGRAM_CHANNEL_ID",-1002134502479))
     SECRET_CODE_LENGTH = int(env.get("SECRET_CODE_LENGTH", 12))
 
 class Server:
-    BASE_URL = env.get("BASE_URL", "http://127.0.0.1:8080")
+    BASE_URL = env.get("BASE_URL", "https://filestream-h-stream.koyeb.app/")
     BIND_ADDRESS = env.get("BIND_ADDRESS", "0.0.0.0")
     PORT = int(env.get("PORT", 8080))
 
