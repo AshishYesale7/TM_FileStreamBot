d30fc0992d54275eb4d424a1b0242b4cd46b0770
Ashish  Vasant Yesale <67554129+AshishYesale7@users.noreply.github.com>
2024-02-04 21:00:32 +0530

    Update config.py
    local host url changed to koyed url https://filestream-h-stream.koyeb.app/

--- bot/config.py
+++ bot/config.py
@@ -11,7 +11,7 @@ class Telegram:
     SECRET_CODE_LENGTH = int(env.get("SECRET_CODE_LENGTH", 12))
 
 class Server:
-    BASE_URL = env.get("BASE_URL", "https://filestream-h-stream.koyeb.app/")
+    BASE_URL = env.get("BASE_URL", "https://filestream-h-stream.koyeb.app")
     BIND_ADDRESS = env.get("BIND_ADDRESS", "0.0.0.0")
     PORT = int(env.get("PORT", 8080))
 
